6f20a6db66826c605e19c13fdb16722b8914d3a1
Olaoye Mich <41102062+michson@users.noreply.github.com>
2022-09-10 00:12:23 +0100

    Update using-ts.js

--- using-ts.js
+++ using-ts.js
@@ -4,8 +4,6 @@ var input2 = document.querySelector("num2");
 function add(num1, num2) {
     return num1 + num2;
 }
-
-
 button.addEventListener("click", function () {
     console.log(add(+input1.value, +input2.value));
 });
